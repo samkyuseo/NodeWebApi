996e92e1223f1271bb9cf05b0dbfe56119682bd2
samkyuseo <lee344@usc.edu>
2020-05-26 08:27:24 -0700

    connection logic t

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
